fdbc3108b24fd11187af9c6c381f2a4d293ece2a
Roman <romanantoshkivvol@gmail.com>
2026-05-06 12:35:43 +0300

    feat: add batch creation and retrieval with auth

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express"; 
 const router = new Router;
 
-import userRouter from './userRouter.js' 
+import userRouter from './userRoutes.js' 
+import batchRouter from './batchRoutes.js' 
 
 router.use('/user', userRouter);
+router.use('/batch', batchRouter);
 
 export default router;
